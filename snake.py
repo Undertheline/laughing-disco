fce2f85ab0ac833dd29a5a9be994b33f26f6940f
Undertheline <105496443+Undertheline@users.noreply.github.com>
2025-07-16 17:52:33 +0000

    massa

--- snake.py
+++ snake.py
@@ -3,19 +3,18 @@ import array
 tela = []
 def draw():
     i = 0
-    while i < 64:
+    while i < 25:
         tela.append("O")
         i += 1
-        if i == 64:
+        if i == 25:
             lista = tela
-            def print_com_limite(texto, limite=8):
+            def print_com_limite(texto, limite=5):
                 for i in range(0, len(texto), limite):
                     print(texto[i:i+limite])
-                    lista.clear
-                    tela.clear
-    print_com_limite(lista, limite=8)
-    
+    print_com_limite(lista, limite=5)
+    lista.clear
+    tela.clear
 
-tela.insert(20, "9") 
-tela.insert(20, "9") 
+draw()
+tela.insert(20, "2") 
 draw()
