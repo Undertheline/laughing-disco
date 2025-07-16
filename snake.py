910e71e4cbbb8c7ce3364f8e9908afb411d9a8e4
Undertheline <105496443+Undertheline@users.noreply.github.com>
2025-07-16 17:48:32 +0000

    massa

--- snake.py
+++ snake.py
@@ -1,1 +1,21 @@
-console.log("aso")
+import array
+
+tela = []
+def draw():
+    i = 0
+    while i < 64:
+        tela.append("O")
+        i += 1
+        if i == 64:
+            lista = tela
+            def print_com_limite(texto, limite=8):
+                for i in range(0, len(texto), limite):
+                    print(texto[i:i+limite])
+                    lista.clear
+                    tela.clear
+    print_com_limite(lista, limite=8)
+    
+
+tela.insert(20, "9") 
+tela.insert(20, "9") 
+draw()
